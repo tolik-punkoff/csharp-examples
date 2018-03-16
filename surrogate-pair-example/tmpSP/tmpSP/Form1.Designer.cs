4930065d604b4e27dd53e71293f989f6e91d4593
tolik-punkoff <tolikgit@inbox.lv>
2018-03-16 03:15:01 +0300

    Surrogate pair displaying example

---
+++ surrogate-pair-example/tmpSP/tmpSP/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace tmpSP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTest = new System.Windows.Forms.TextBox();
+            this.rtbTest = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTest = new System.Windows.Forms.Label();
+            this.btnMessageBox = new System.Windows.Forms.Button();
+            this.btnFont = new System.Windows.Forms.Button();
+            this.lblFont = new System.Windows.Forms.Label();
+            this.dlgFont = new System.Windows.Forms.FontDialog();
+            this.SuspendLayout();
+            // 
+            // txtTest
+            // 
+            this.txtTest.Location = new System.Drawing.Point(77, 3);
+            this.txtTest.Name = "txtTest";
+            this.txtTest.Size = new System.Drawing.Size(100, 20);
+            this.txtTest.TabIndex = 0;
+            // 
+            // rtbTest
+            // 
+            this.rtbTest.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.rtbTest.Location = new System.Drawing.Point(77, 29);
+            this.rtbTest.Name = "rtbTest";
+            this.rtbTest.Size = new System.Drawing.Size(100, 36);
+            this.rtbTest.TabIndex = 1;
+            this.rtbTest.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "TextBox";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "RichTextBox";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Label:";
+            // 
+            // lblTest
+            // 
+            this.lblTest.AutoSize = true;
+            this.lblTest.Location = new System.Drawing.Point(74, 76);
+            this.lblTest.Name = "lblTest";
+            this.lblTest.Size = new System.Drawing.Size(97, 13);
+            this.lblTest.TabIndex = 5;
+            this.lblTest.Text = "Surrogate pair here";
+            // 
+            // btnMessageBox
+            // 
+            this.btnMessageBox.Location = new System.Drawing.Point(6, 110);
+            this.btnMessageBox.Name = "btnMessageBox";
+            this.btnMessageBox.Size = new System.Drawing.Size(79, 23);
+            this.btnMessageBox.TabIndex = 6;
+            this.btnMessageBox.Text = "MessageBox";
+            this.btnMessageBox.UseVisualStyleBackColor = true;
+            this.btnMessageBox.Click += new System.EventHandler(this.btnMessageBox_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Location = new System.Drawing.Point(115, 110);
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(75, 23);
+            this.btnFont.TabIndex = 7;
+            this.btnFont.Text = "Font";
+            this.btnFont.UseVisualStyleBackColor = true;
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // lblFont
+            // 
+            this.lblFont.AutoSize = true;
+            this.lblFont.Location = new System.Drawing.Point(74, 149);
+            this.lblFont.Name = "lblFont";
+            this.lblFont.Size = new System.Drawing.Size(28, 13);
+            this.lblFont.TabIndex = 8;
+            this.lblFont.Text = "Font";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(196, 273);
+            this.Controls.Add(this.lblFont);
+            this.Controls.Add(this.btnFont);
+            this.Controls.Add(this.btnMessageBox);
+            this.Controls.Add(this.lblTest);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.rtbTest);
+            this.Controls.Add(this.txtTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTest;
+        private System.Windows.Forms.RichTextBox rtbTest;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTest;
+        private System.Windows.Forms.Button btnMessageBox;
+        private System.Windows.Forms.Button btnFont;
+        private System.Windows.Forms.Label lblFont;
+        private System.Windows.Forms.FontDialog dlgFont;
+    }
+}
+
---
+++ surrogate-pair-example/tmpSP/tmpSP/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace tmpSP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int Code = 0x14476; //анатолийский иероглиф
+        string myString = "In the game of mahjong \U0001F01C denotes the Four of circles";
+        Font AnFont = new Font("Anatolian", 24, FontStyle.Regular, GraphicsUnit.Pixel, 1);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strSP = char.ConvertFromUtf32(Code);
+            
+            lblTest.Font = AnFont;
+            txtTest.Font = AnFont;                        
+            lblFont.Text = AnFont.FontFamily.Name;
+            rtbTest.Font = AnFont;
+
+            lblTest.Text = strSP;
+            txtTest.Text = strSP;
+            rtbTest.Text = strSP;
+
+            
+
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            DialogResult Ans = dlgFont.ShowDialog();
+            if (Ans == DialogResult.Cancel) return;
+            
+
+            lblTest.Font = dlgFont.Font;
+            txtTest.Font = dlgFont.Font;
+            rtbTest.Font = dlgFont.Font;
+            lblFont.Text = dlgFont.Font.Name;
+        }
+
+        private void btnMessageBox_Click(object sender, EventArgs e)
+        {
+            string strSP = char.ConvertFromUtf32(Code);
+            MessageBox.Show(strSP);
+        }
+    }
+}
