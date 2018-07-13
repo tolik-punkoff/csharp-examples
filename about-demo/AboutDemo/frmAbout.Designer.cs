082d4bc68058189d9b9b85890792852126db3cbb
Tolik Punkoff <tolikgit@inbox.lv>
2018-07-13 11:57:44 +0300

    small bug fixed and add logo

--- about-demo/AboutDemo/frmAbout.Designer.cs
+++ about-demo/AboutDemo/frmAbout.Designer.cs
@@ -41,9 +41,10 @@
             // btnOK
             // 
             this.btnOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnOK.Location = new System.Drawing.Point(119, 448);
+            this.btnOK.Location = new System.Drawing.Point(159, 551);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4);
             this.btnOK.Name = "btnOK";
-            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
             this.btnOK.TabIndex = 0;
             this.btnOK.Text = "OK";
             this.btnOK.UseVisualStyleBackColor = true;
@@ -51,11 +52,14 @@
             // 
             // pctLogo
             // 
+            this.pctLogo.BackgroundImage = global::AboutDemo.Properties.Resources.logo;
+            this.pctLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pctLogo.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.pctLogo.Image = ((System.Drawing.Image)(resources.GetObject("pctLogo.Image")));
-            this.pctLogo.Location = new System.Drawing.Point(16, 24);
+            this.pctLogo.InitialImage = null;
+            this.pctLogo.Location = new System.Drawing.Point(21, 30);
+            this.pctLogo.Margin = new System.Windows.Forms.Padding(4);
             this.pctLogo.Name = "pctLogo";
-            this.pctLogo.Size = new System.Drawing.Size(291, 72);
+            this.pctLogo.Size = new System.Drawing.Size(388, 89);
             this.pctLogo.TabIndex = 1;
             this.pctLogo.TabStop = false;
             this.pctLogo.Click += new System.EventHandler(this.pctLogo_Click);
@@ -63,9 +67,10 @@
             // lblProgName
             // 
             this.lblProgName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblProgName.Location = new System.Drawing.Point(12, 0);
+            this.lblProgName.Location = new System.Drawing.Point(16, 0);
+            this.lblProgName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblProgName.Name = "lblProgName";
-            this.lblProgName.Size = new System.Drawing.Size(305, 20);
+            this.lblProgName.Size = new System.Drawing.Size(407, 25);
             this.lblProgName.TabIndex = 2;
             this.lblProgName.Text = "About window demo";
             this.lblProgName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -73,9 +78,10 @@
             // lblCopyright
             // 
             this.lblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.lblCopyright.Location = new System.Drawing.Point(4, 423);
+            this.lblCopyright.Location = new System.Drawing.Point(5, 521);
+            this.lblCopyright.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblCopyright.Name = "lblCopyright";
-            this.lblCopyright.Size = new System.Drawing.Size(317, 50);
+            this.lblCopyright.Size = new System.Drawing.Size(423, 62);
             this.lblCopyright.TabIndex = 3;
             this.lblCopyright.Text = "(L) Hex_Laden [Wildsoft], 2010-2018";
             this.lblCopyright.TextAlign = System.Drawing.ContentAlignment.TopCenter;
@@ -83,17 +89,18 @@
             // pctMessages
             // 
             this.pctMessages.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.pctMessages.Location = new System.Drawing.Point(3, 105);
+            this.pctMessages.Location = new System.Drawing.Point(4, 129);
+            this.pctMessages.Margin = new System.Windows.Forms.Padding(4);
             this.pctMessages.Name = "pctMessages";
-            this.pctMessages.Size = new System.Drawing.Size(318, 319);
+            this.pctMessages.Size = new System.Drawing.Size(424, 393);
             this.pctMessages.TabIndex = 4;
             this.pctMessages.TabStop = false;
             // 
             // frmAbout
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(322, 475);
+            this.ClientSize = new System.Drawing.Size(429, 585);
             this.Controls.Add(this.pctMessages);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.lblCopyright);
@@ -102,6 +109,7 @@
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmAbout";
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
