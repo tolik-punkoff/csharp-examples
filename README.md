e759cc6ed4c8907ae1a03e089e81f8f97d635238
Tolik Punkoff <tolikgit@inbox.lv>
2019-09-03 03:50:17 +0300

    Add get user profile folder example

--- README.md
+++ README.md
@@ -46,3 +46,5 @@ spec-diaps - checks if the IP-address is in a special-use addresses diapasons.
 create-shortcut - Create Windows shortcut (LNK) example (with WSHShell)
 
 os-version - OS version, name, service pack and architecture detect example
+
+get-user-profile-folder-example - Get user profile folder (e.g. C:\users\UserName) example
---
+++ get-user-profile-folder-example/GetUserProfileFolderExample/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace GetUserProfileFolderExample
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            string UserProfile = Environment.GetEnvironmentVariable("USERPROFILE");
+
+            MessageBox.Show(UserProfile, "User profile folder path",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
